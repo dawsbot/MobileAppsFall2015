9ddedc5e210722de39c22f008cd737cc0f5c9608
dawsonbotsford <dawsonbotsford@gmail.com>
2015-09-29 10:33:47 -0600

    Bfriendly init

---
+++ iOS/Bfriendly/Bfriendly/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Bfriendly
+//
+//  Created by Dawson Botsford on 9/29/15.
+//  Copyright © 2015 Dawson Botsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
