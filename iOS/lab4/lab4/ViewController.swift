a80b5faff4dfe8fbc2ca0325541aab394866a5d8
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-10-06 10:12:14 -0600

    lab4 init

---
+++ iOS/lab4/lab4/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  lab4
+//
+//  Created by Dawson Botsford on 10/6/15.
+//  Copyright © 2015 Dawson Botsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var delta = CGPointMake(12, 4)
+    var ballRadius = CGFloat()
+    var timer = NSTimer()
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var sliderLabel: UILabel!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        timer.invalidate()
+        changeSliderView()
+    }
+    
+    func moveImage() {
+        imageView.center = CGPointMake(imageView.center.x + delta.x, imageView.center.y + delta.y)
+        
+        if imageView.center.x > view.bounds.size.width - ballRadius || imageView.center.x < ballRadius {
+            delta.x = -delta.x
+        }
+        if imageView.center.y > view.bounds.size.width - ballRadius || imageView.center.y < ballRadius {
+            delta.y = -delta.y
+        }
+    }
+    
+    func changeSliderView() {
+        sliderLabel.text = String(format: "%.2f", slider.value)
+        timer = NSTimer.scheduledTimerWithTimeInterval(Double(slider.value), target: self, selector: "moveImage", userInfo: nil, repeats: true)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        ballRadius = imageView.frame.size.width/2
+        changeSliderView()
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
