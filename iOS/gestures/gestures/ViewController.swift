c20fb46b14fe8dc9f512afbe452c8e0e9cbecb47
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-10-13 10:09:21 -0600

    decelerating sliding complete

--- iOS/gestures/gestures/ViewController.swift
+++ iOS/gestures/gestures/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         let translation = sender.translationInView(view)
         sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
         sender.setTranslation(CGPointZero, inView: view)
+        if sender.state == UIGestureRecognizerState.Ended {
+            let velocity = sender.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            
+            let slideFactor = 0.1 * slideMultiplier
+            
+            var finalPoint = CGPoint(x:sender.view!.center.x + (velocity.x * slideFactor), y: sender.view!.center.y + (velocity.y * slideFactor))
+            
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {sender.view!.center =  finalPoint }, completion: nil)
+    
+            
+        
+        }
         
     }
     
