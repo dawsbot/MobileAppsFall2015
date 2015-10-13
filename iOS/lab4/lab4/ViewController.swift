9fcc7170ad104d0341a6be87010e018629156732
dawsonbotsford <dawsonbotsford@gmail.com>
2015-10-12 22:51:10 -0600

    Ball bouncing done. appicon and horizontal to go

--- iOS/lab4/lab4/ViewController.swift
+++ iOS/lab4/lab4/ViewController.swift
@@ -7,11 +7,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var delta = CGPointMake(0, 10) //initialize the delta to move 0 pixels horizontally, 10 pixels vertically
+    var delta = CGPointMake(0.0, 12) //initialize the delta to move 0 pixels horizontally, 10 pixels vertically
     var ballRadius = CGFloat() //radius of the ball image
     var timer = NSTimer() //animation timer
     var translation = CGPointMake(0.0, 0.0) //specifies how many pixels the image will move
     var angle:CGFloat=0.0 //roation angle
+    var reboundRate:CGFloat = 1.0
+    var spinIt = true
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var sliderLabel: UILabel!
@@ -26,33 +28,44 @@ class ViewController: UIViewController {
     func moveImage() {
         let duration=Double(slider.value)
         UIView.beginAnimations("bball", context: nil)
-        UIView.animateWithDuration(duration, animations:
-            {self.imageView.transform=CGAffineTransformMakeRotation(self.angle)
-                self.imageView.center = CGPointMake(self.imageView.center.x + self.delta.x, self.imageView.center.y + self.delta.y)
-        })
-        UIView.commitAnimations()
+       
+        if (spinIt) {
+            UIView.animateWithDuration(duration, animations:
+                {self.imageView.transform=CGAffineTransformMakeRotation(self.angle)
+                    self.imageView.center = CGPointMake(self.imageView.center.x + self.delta.x, self.imageView.center.y + self.delta.y)
+            })
+            UIView.commitAnimations()
+        }
         angle += 0.07 //amount you increase the angle
         //if it's a full rotation reset the angle
         if angle > CGFloat(2*M_PI){
             angle=0
         }
+        delta.y += 0.7
         if imageView.center.x > self.view.bounds.size.width-ballRadius || imageView.center.x < ballRadius{
             delta.x = -delta.x
         }
         if imageView.center.y  > self.view.bounds.size.height - ballRadius || imageView.center.y  < ballRadius {
             delta.y = -delta.y
+            if (delta.y < 0) {
+                delta.y += 2;
+            }
+            if (abs(delta.y) < 3) {
+                delta.y = 0
+                spinIt = false
+            }
         }
     }
     
     //updates the timer and label with the current slider value
     @IBAction func changeSliderValue() {
         sliderLabel.text=String(format: "%.2f", slider.value)
-        timer = NSTimer.scheduledTimerWithTimeInterval(Double(slider.value / 14), target: self, selector: "moveImage", userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(Double(slider.value / 34), target: self, selector: "moveImage", userInfo: nil, repeats: true)
     }
     
     override func viewDidLoad() {
         //ball radius is half the width of the image
-        ballRadius=imageView.frame.size.width/3
+        ballRadius=imageView.frame.size.width/2
         changeSliderValue()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
