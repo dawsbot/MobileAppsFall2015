980d81fa84126d247c6526a1490f2d535a8b6135
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-10-13 09:44:15 -0600

    npm womat init

---
+++ iOS/gestures/gestures/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  gestures
+//
+//  Created by Dawson Botsford on 10/13/15.
+//  Copyright © 2015 Dawson Botsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func handlePan(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(view)
+        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        sender.setTranslation(CGPointZero, inView: view)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
