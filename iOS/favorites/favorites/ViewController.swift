b023387595a00492a7f88a4b5f0754c2ef04a32f
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-10-20 10:20:18 -0600

    Segues functioning

---
+++ iOS/favorites/favorites/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  favorites
+//
+//  Created by Dawson Botsford on 10/20/15.
+//  Copyright © 2015 Dawson Botsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func unwindSegue (segue:UIStoryboardSegue){
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
