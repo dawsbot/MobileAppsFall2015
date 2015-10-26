c7843f1c1ee55f14c16d574d95f9a2d6d5ab826f
dawsonbotsford <dawsonbotsford@gmail.com>
2015-10-26 16:06:40 -0600

    briend Part 2 contacts integrated. Not saved yet

---
+++ iOS/casualMessage/casualMessageTests/casualMessageTests.swift
@@ -0,0 +1,36 @@
+//
+//  casualMessageTests.swift
+//  casualMessageTests
+//
+//  Created by Dawson Botsford on 10/22/15.
+//  Copyright (c) 2015 Dawson Botsford. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class casualMessageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
