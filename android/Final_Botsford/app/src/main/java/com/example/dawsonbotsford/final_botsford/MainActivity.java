df14354bb6069f5c95dd3dd6c61b1d62b0740744
dawsonbotsford <dawsonbotsford@gmail.com>
2015-12-10 09:36:47 -0700

    slider complete. 60 pts

--- android/Final_Botsford/app/src/main/java/com/example/dawsonbotsford/final_botsford/MainActivity.java
+++ android/Final_Botsford/app/src/main/java/com/example/dawsonbotsford/final_botsford/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,17 +15,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Get's the name for this pizza from the edit text at top of app
+    private String getPizzaName(){
+        EditText pizza_Name_EditText = (EditText) findViewById(R.id.pizzaName);
+        return pizza_Name_EditText.getText().toString();
+    }
+
+    //Get's the user's choice for gluten free or not from the slider
+    private String getGlutenFree(){
+        Switch gf_Switch = (Switch) findViewById(R.id.gluten_free);
+        boolean choice = gf_Switch.isChecked();
+
+        if (choice) {
+            return "is a gluten free pizza";
+        }
+        else {
+            return "is a gluent-free's ENEMY";
+        }
+    }
+
     public void buildPizza(View v){
         String phrase;
 
-        EditText pizza_Name_EditText = (EditText) findViewById(R.id.pizzaName);
-        String pizzaName = pizza_Name_EditText.getText().toString();
-
-        System.out.println(pizzaName);
-
-        phrase = "Your pizza is called the " + pizzaName;
+        phrase = "Your pizza is called the " + getPizzaName() + ". It " + getGlutenFree();
 
         TextView final_phrase_EditText = (TextView) findViewById(R.id.finalPhrase);
         final_phrase_EditText.setText(phrase);
+
+
+
     }
 }
