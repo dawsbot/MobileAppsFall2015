17a1e2a86e522cd79ee8acfc179c7777bffc1604
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-12-09 21:05:07 -0700

    explicity intent complete

--- android/Lab10/app/src/main/java/com/example/dawsonbotsford/lab10/ReceiveCityActivity.java
+++ android/Lab10/app/src/main/java/com/example/dawsonbotsford/lab10/ReceiveCityActivity.java
@@ -1,8 +1,10 @@
 package com.example.dawsonbotsford.lab10;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ReceiveCityActivity extends AppCompatActivity {
@@ -21,10 +23,14 @@ public class ReceiveCityActivity extends AppCompatActivity {
 
         TextView messageView = (TextView) findViewById(R.id.city_name);
         System.out.println (cityName);
-        System.out.println (cityURL);
+        System.out.println(cityURL);
         messageView.setText(cityName);
 
+    }
 
-
+    public void loadWebSite(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(cityURL));
+        startActivity(intent);
     }
 }
